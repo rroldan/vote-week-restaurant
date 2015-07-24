0d32d15d578f2adfb6574e0e59ec5f5c4351f3ba
Ricardo Roldán Hernández <roldan.ricardo@gmail.com>
2015-07-24 19:55:17 +0200

    add log4j

--- src/main/java/org/taxidermia/voteweekrestaurant/application/VoteService.java
+++ src/main/java/org/taxidermia/voteweekrestaurant/application/VoteService.java
@@ -1,18 +1,20 @@
 package org.taxidermia.voteweekrestaurant.application;
 
+import org.apache.log4j.Logger;
 import org.taxidermia.voteweekrestaurant.model.Vote;
 import org.taxidermia.voteweekrestaurant.model.VoteRepository;
+
 
 /**
  * implementeacion la logica de Voto
  */
 public class VoteService {
+    static Logger logger = Logger.getLogger(VoteService.class);
 
     private VoteRepository voteRepository;
 
     public VoteService(VoteRepository newVoteRepository){
         this.voteRepository = newVoteRepository;
-
     }
 
 
@@ -21,6 +23,7 @@ public class VoteService {
      * @param vote Voto realizado
      */
     public void vote(Vote vote){
+        logger.info("vote()" + vote.toString() );
         voteRepository.save(vote);
 
     }
